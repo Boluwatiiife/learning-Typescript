b7b88ca7fd2def55101815d0eca36594cfb399e9
Boluwatiiife <tundelawalb@gmail.com>
2023-07-17 13:27:20 +0100

    .

---
+++ greet.js
@@ -0,0 +1,6 @@
+'use strict';
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+}
+greet('Boluwatife', new Date());
+console.log('i love coding');
